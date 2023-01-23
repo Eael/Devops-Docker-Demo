3946345fe5bbdb280277d03443d5151e1fc85a1d
Eael <teyeearl@gmail.com>
2023-01-23 14:46:38 +0000

    Added a new simple_function to call

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-import os
+def simple_function(msg):
+    print(msg)
+
 
 def myfunction():
     print(True)
-    return 1
+    return simple_function("Everything is ok")
+
+
 
 print(myfunction())
